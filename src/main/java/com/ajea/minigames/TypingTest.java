e62bf4814b2c9a5c3077300768048132c8b0a08f
Jason LaPierre <jason.lapie@gmail.com>
2022-03-25 22:41:53 -0400

    getSkill()

--- src/main/java/com/ajea/minigames/TypingTest.java
+++ src/main/java/com/ajea/minigames/TypingTest.java
@@ -1,8 +1,12 @@
 package com.ajea.minigames;
 
+import com.ajea.Enums.SkillLevel;
+
 public class TypingTest {
     int score = 0;
 
 
-
+    public SkillLevel getSkill() {
+        return SkillLevel.HIGH; // TODO: Make dynamic
+    }
 }
