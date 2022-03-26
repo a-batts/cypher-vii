e4ed5e47ae4d2a4499a36cda5f10d18553405ee2
Alex Batts <alex.batts05@gmail.com>
2022-03-26 12:41:38 -0400

    Switched to new class level scanner

--- src/main/java/com/ajea/Main.java
+++ src/main/java/com/ajea/Main.java
@@ -1,6 +1,9 @@
 package com.ajea;
 
+import java.util.Scanner;
+
 public class Main {
+    public static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args){
         //Main class
