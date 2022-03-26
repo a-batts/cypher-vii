4c986753d51362ffd29bdda64f19b201d1ebfbe3
Alex Batts <alex.batts05@gmail.com>
2022-03-26 03:59:04 -0400

    Cleaned up main class

--- src/main/java/com/ajea/Main.java
+++ src/main/java/com/ajea/Main.java
@@ -1,13 +1,9 @@
 package com.ajea;
 
-import com.ajea.minigames.TypingTest;
-
 public class Main {
 
     public static void main(String[] args){
         //Main class
-        TypingTest typingTest = new TypingTest();
-        System.out.println(typingTest.start());
 
     }
 }
