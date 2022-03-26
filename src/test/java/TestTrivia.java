dd9045f360ac7cf734b6cef21cd9344d17ecd8ad
Jason LaPierre <jason.lapie@gmail.com>
2022-03-26 16:40:10 -0400

    Deleted test files
    Signed-off-by: Jason LaPierre <jason.lapie@gmail.com>

--- src/test/java/TestTrivia.java
+++
@@ -1,9 +0,0 @@
-import com.ajea.enums.SkillLevel;
-import com.ajea.minigames.trivia.Trivia;
-
-public class TestTrivia {
-    public static void main(String[] args){
-        Trivia t = new Trivia();
-        SkillLevel l = t.start();
-    }
-}
--- src/test/java/TestTypingTest.java
+++
@@ -1,7 +0,0 @@
-import com.ajea.minigames.TypingTest;
-
-public class TestTypingTest {
-    public static void main(String[] args){
-        TypingTest test = new TypingTest();
-    }
-}
