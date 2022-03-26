7f1d62b399717305310ca89f70fcd4e62894754e
Jason LaPierre <jason.lapie@gmail.com>
2022-03-26 16:35:28 -0400

    Added static colors
    Signed-off-by: Jason LaPierre <jason.lapie@gmail.com>

--- src/main/java/com/ajea/Main.java
+++ src/main/java/com/ajea/Main.java
@@ -7,6 +7,13 @@ import com.ajea.minigames.trivia.Trivia;
 import java.util.*;
 
 public class Main {
+    // Color Constants of Console Text
+    public static final String ANSI_RESET = "\u001B[0m"; // Reset to consoles default.
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
     public static Scanner scan = new Scanner(System.in);
 
     public static final Player PLAYER = new Player();
