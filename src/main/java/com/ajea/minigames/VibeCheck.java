3ad409d7000d21013381b936c2798603ad34d8b1
Angela <asully88865@gmail.com>
2022-03-26 16:04:09 -0400

    Commiting Vibe Check :)

--- src/main/java/com/ajea/minigames/VibeCheck.java
+++ src/main/java/com/ajea/minigames/VibeCheck.java
@@ -1,10 +1,61 @@
 package com.ajea.minigames;
 
 import com.ajea.enums.SkillLevel;
+import com.ajea.Prompt;
 
-public class VibeCheck implements Minigame {
+import javax.swing.*;
+import java.awt.*;
+
+public class VibeCheck extends JPanel implements Minigame{
+    private String[] images = {};
+    private static int maxHeight = 1500;
+    private static int maxWidth = 900;
+    private String[] prompts = {};
+    private String[][] options = {{}};
+    private int answers[] = {};
+    private int increm = 0;
+
+    public VibeCheck(){
+    }
+
     @Override
     public SkillLevel start() {
         return null;
     }
+
+    public void paintComponent(Graphics g){
+        ImageIcon senario = new ImageIcon(images[increm]);
+        g.drawImage(senario.getImage(), 0,0, maxWidth, maxHeight, null);
+
+    }
+
+    private boolean test(String prompt, String[] options, int answer){
+        Prompt question = new Prompt();
+        int value = question.promptUser(prompt,options);
+        if (value == answer){
+            return true;
+        }
+        return false;
+    }
+
+    public int checkVibe(){
+        int score = 0;
+        while(increm<3){
+            if(test(prompts[increm], options[increm], answers[increm])){
+                score++;
+            }
+            increm++;
+            repaint();
+        }
+        return score;
+    }
+
+    public SkillLevel skillLevel(){
+        int skill = checkVibe();
+        if(skill == 3) return SkillLevel.HIGH;
+        else if (skill == 2) return SkillLevel.MEDIUM;
+        else return SkillLevel.LOW;
+    }
+
 }
+
