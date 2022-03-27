8a4f27f5cd0e0b2dfc42d2e06afbe4ef5d9d69ee
Alex Batts <alex.batts05@gmail.com>
2022-03-27 08:03:24 -0400

    Dating Sim fixed compatibility

--- src/main/java/com/ajea/minigames/datingSim/DatingQuestion.java
+++ src/main/java/com/ajea/minigames/datingSim/DatingQuestion.java
@@ -1,4 +1,56 @@
 package com.ajea.minigames.datingSim;
 
-public record DatingQuestion(String question, String[] options) {
+import java.util.Arrays;
+import java.util.Objects;
+
+public final class DatingQuestion {
+    private final String question;
+    private final String[] options;
+    private final int correctOption;
+
+    public DatingQuestion(String question, String[] options) {
+        this.question = question;
+        this.options = options;
+        this.correctOption = 0;
+    }
+
+    public boolean checkAnswer(int answer) {
+        return answer == correctOption;
+    }
+
+    public String question() {
+        return question;
+    }
+
+    public String[] options() {
+        return options;
+    }
+
+    public int correctOption() {
+        return correctOption;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (DatingQuestion) obj;
+        return Objects.equals(this.question, that.question) &&
+                Objects.equals(this.options, that.options) &&
+                this.correctOption == that.correctOption;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(question, Arrays.hashCode(options), correctOption);
+    }
+
+    @Override
+    public String toString() {
+        return "DatingQuestion[" +
+                "question=" + question + ", " +
+                "options=" + options + ", " +
+                "correctOption=" + correctOption + ']';
+    }
+
 }
--- src/main/java/com/ajea/minigames/datingSim/DatingSim.java
+++ src/main/java/com/ajea/minigames/datingSim/DatingSim.java
@@ -1,6 +1,7 @@
 package com.ajea.minigames.datingSim;
 
 import com.ajea.enums.SkillLevel;
+import com.ajea.minigames.Minigame;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,7 +9,7 @@ import java.util.List;
 import static com.ajea.Main.*;
 import static com.ajea.Prompt.promptUser;
 
-public class DatingSim {
+public class DatingSim implements Minigame {
     private DatingQuestion[] datingQuestions;
     int score = 0;
     final public String gameSkill = "charm";
@@ -75,15 +76,12 @@ public class DatingSim {
                 int answer4 = promptUser(datingQuestions[currentQuestion].question(), datingQuestions[currentQuestion].options());
                 if (answer4 == 1) {
                     currentQuestion = 99; // Game ends but wrong choice.
-                    System.out.println("You see a movie with your crush. It’s mildly entertaining, but you quickly learn you’ve made a mistake as you realize that actually talking to your crush would be much more fun than sitting in the dark with them for two hours. You continue to hang out with them often, but just as friends – nothing more. Maybe it’s for the better! Total score: 10/15");
                 } else if (answer4 == 2) {
                     currentQuestion = 99; // Game ends but wrong choice.
                     score += 2;
-                    System.out.println("You and your crush go to Noodles & Co. You have a good time eating your pasta, but you get some sauce on your shirt! Regardless, you make plans for another date with your crush, and you can’t wait to go out with them again. Congratulations! Total score: 12/15");
                 } else if (answer4 == 3) {
                     currentQuestion = 99; // Game ends, FANTASTIC CHOICE!
                     score += 5;
-                    System.out.println("You and your crush go to the farmers market, and it couldn’t be more perfect. The weather is beautiful, and both of you have a lovely time. The date goes wonderfully, and you’ve even made plans for the next one by the end! Congratulations, you’ve wooed your crush! Total score: 15/15");
                 }
             }
         }
@@ -111,32 +109,32 @@ public class DatingSim {
         questions.add(
                 new DatingQuestion("You notice someone really cute across the room in one of your classes, and you really want to get to know them a little better. They approach you at the end of class and ask if you want to study for the next exam together. How do you reply?",
                 new String[] {
-                        "“I’m feeling confident on these topics, so I think I’ll be okay on my own. Maybe we could hang out some other time though!”",
-                        "“Yeah, that sounds good! I’m really confident for this one, so I can give you some pointers to help you do well on the exam.”",
-                        "“That sounds really fun! Want to meet at Swem at 4:00?”"
+                        "\"I’m feeling confident on these topics, so I think I’ll be okay on my own. Maybe we could hang out some other time though!\"",
+                        "\"Yeah, that sounds good! I’m really confident for this one, so I can give you some pointers to help you do well on the exam.\"",
+                        "\"That sounds really fun! Want to meet at Swem at 4:00?\""
                 })
         );
         questions.add(
                 new DatingQuestion("You study with your crush at Swem, and things seem to be going well. You take the exam feeling as confident as ever, and when you get it back, you see that you got a 92. Your crush approaches you excitedly after class and tells you that they got a 98 on the exam. How do you respond?",
                 new String[] {
-                        "“Looks like next time you’ll be the one giving me pointers!”",
-                        "“Wow, you must have gotten lucky with the partial credit! Nice job!”",
-                        "“I guess you didn’t need me after all!”"}));
+                        "\"Looks like next time you’ll be the one giving me pointers!\"",
+                        "\"Wow, you must have gotten lucky with the partial credit! Nice job!\"",
+                        "\"I guess you didn’t need me after all!\""}));
 
         questions.add(
                 new DatingQuestion("You study with your crush at Swem, and you really hit it off. You leave the study session feeling perfectly prepared, and you’re confident when you take the exam. When you get the exam back, you’re overjoyed to learn that both you and your crush knocked it out of the park! You want to ask them to go celebrate somewhere with you. What do you say?",
                 new String[] {
-                        "“Wanna get smoothies from Wawa with me?”",
-                        "“I hear that froyo place near New Town is good, do you want to try it with me?”",
-                        "“I hear Caf has good brownies today, let’s go there!”"
+                        "\"Wanna get smoothies from Wawa with me?\"",
+                        "\"I hear that froyo place near New Town is good, do you want to try it with me?\"",
+                        "\"I hear Caf has good brownies today, let’s go there!\""
                 }));
 
         questions.add(
                 new DatingQuestion("You go to Wawa and get your smoothies. Your crush really enjoyed theirs, and wants to hang out again sometime! What do you suggest?",
                 new String[] {
-                        "“Let’s go see a movie this Saturday!”",
-                        "“Let’s go check out the Williamsburg Farmers Market this weekend!”",
-                        "“Let’s go to Noodles & Co. in New Town!”"
+                        "\"Let’s go see a movie this Saturday!\"",
+                        "\"Let’s go check out the Williamsburg Farmers Market this weekend!\"",
+                        "\"Let’s go to Noodles & Co. in New Town!\""
                 })
         );
 
