548005366f28a24efb43ab1d6be3524c729de54f
Alex Batts <alex.batts05@gmail.com>
2022-03-26 22:47:23 -0400

    Restructure of choice selection

--- src/main/java/com/ajea/gametree/GameTree.java
+++ src/main/java/com/ajea/gametree/GameTree.java
@@ -1,26 +1,21 @@
 package com.ajea.gametree;
 
 import com.ajea.FileLoader;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
+import com.ajea.Main;
 import com.google.gson.JsonObject;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class GameTree {
-    private JsonObject choices;
+    private final JsonObject data;
     private final TreeNode root = new TreeNode();
 
     public GameTree(String fileName){
         FileLoader fl = new FileLoader().loadJSON(fileName);
-        choices = fl.asJsonObject();
+        data = fl.asJsonObject();
     }
 
     public GameTree initTree(){
         //Initialize first node
-        TreeNode current = this.root;
-        setData(current, 1);
+        setData(this.root, 1);
         //Initialize the rest of the story line once it is done
 
         return this;
@@ -32,19 +27,22 @@ public class GameTree {
      * @param index index of JSON line
      */
     private void setData(TreeNode node, int index){
-        String prompt = choices.get(Integer.toString(index)).getAsJsonArray().get(0).getAsJsonObject().get("prompt").toString();
-        Choice[] choices = getChoices(index);
-        node.setData(prompt, choices);
+        node.setData(getPrompt(index), getChoices(index));
     }
 
-    private Choice[] getChoices(int index){
-        JsonArray currentObject = choices.get(Integer.toString(index)).getAsJsonArray().get(0).getAsJsonObject().get("choices").getAsJsonArray();
-        ArrayList<Choice> choices = new ArrayList<>();
-        for(JsonElement e: currentObject){
-            JsonObject arr = e.getAsJsonObject();
-            choices.add(new Choice(arr.get("skill").toString()).setChoices(arr.get("short").toString(), arr.get("medium").toString(), arr.get("long").toString()));
-        }
+    private String getPrompt(int index){
+        JsonObject promptData = data.get(Integer.toString(index)).getAsJsonArray().get(0).getAsJsonObject().get("prompts").getAsJsonArray().get(0).getAsJsonObject();
 
-        return choices.toArray(new Choice[0]);
+        return switch (Main.PLAYER.getLevelOfSkill(promptData.get("skill").getAsString()).getLevelInt()) {
+            case 2, 3 -> promptData.get("long").toString();
+            case 4, 5 -> promptData.get("medium").toString();
+            default -> promptData.get("short").toString();
+        };
+    }
+
+    private String[] getChoices(int index){
+        JsonObject choiceData = data.get(Integer.toString(index)).getAsJsonArray().get(0).getAsJsonObject().get("choices").getAsJsonArray().get(0).getAsJsonObject();
+
+        return new String[]{choiceData.get("a").toString(), choiceData.get("b").toString()};
     }
 }
--- src/main/java/com/ajea/gametree/TreeNode.java
+++ src/main/java/com/ajea/gametree/TreeNode.java
@@ -4,14 +4,13 @@ import static com.ajea.Main.PLAYER;
 
 public class TreeNode {
     private TreeNode left = null;
-    private TreeNode middle = null;
     private TreeNode right = null;
 
     //Optional game dialog displayed before prompt
     public String dialog;
     //Prompt and choices for game action node
     public String prompt;
-    public Choice[] choices;
+    public String[] choices;
 
     public TreeNode(){}
 
@@ -36,16 +35,8 @@ public class TreeNode {
         this.right = right;
     }
 
-    public void setData(String prompt, Choice[] choices){
+    public void setData(String prompt, String[] choices){
         this.prompt = prompt;
         this.choices = choices;
-    }
-
-    public String[] getChoices(){
-        String[] finalChoices = new String[choices.length];
-        for(int i = 0; i < choices.length; i++)
-            finalChoices[i] = choices[i].getChoiceForSkillLevel(PLAYER.getLevelOfSkill(choices[i].skill));
-
-        return finalChoices;
     }
 }
